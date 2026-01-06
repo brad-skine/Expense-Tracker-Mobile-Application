7ee692e4bba9f046f51a799661ad3ca62826d1d5
brad-skine <138541961+brad-skine@users.noreply.github.com>
2026-01-06 16:54:53 +1300

    capacitor settings changes

---
+++ frontend_basic_setup/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bradskine.expensetracker',
+  appName: 'Expense Tracker ',
+  webDir: 'dist/app-v0.1/browser'
+};
+
+export default config;
